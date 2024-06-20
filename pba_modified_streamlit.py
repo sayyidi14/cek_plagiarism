90982b44abac643eff4798e7d2f39a97d0f32d9e
sayyidi14 <111933942+sayyidi14@users.noreply.github.com>
2024-06-20 21:54:55 +0700

    Update pba_modified_streamlit.py

--- pba_modified_streamlit.py
+++ pba_modified_streamlit.py
@@ -6,9 +6,5 @@ import nltk
 import streamlit as st
 
 
-# Inisialisasi stemmer dan stopwords
-stemmer = PorterStemmer()
-stop_words = set(stopwords.words('indonesian'))
-
 df= pd.read_csv("coba.csv")
 df
